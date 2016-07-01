a0ffcec5b01a23e90af0564598b32c11ebc2533f
bingo <616800976@qq.com>
2016-07-01 20:38:57 +0800

    bingo
    完成

--- ViewController.m
+++ ViewController.m
@@ -13,7 +13,6 @@
 #import <QuartzCore/QuartzCore.h>
 
 
-
 typedef enum {
     isFrontCamera = 0,
     isBackCamera
@@ -72,31 +71,124 @@ typedef enum {
     [self.session stopRunning];
     
     [_previewLayer removeFromSuperlayer];
-    
-    sender.selected  = !_isFontCamera;
+    sender.selected  = !sender.selected;
+    _isFontCamera = !_isFontCamera;
     [self setupCamera];
 }
 
 //获取图片
 - (void)getPhoto{
+    AVCaptureConnection *videoConnection = nil;
+    for (AVCaptureConnection *connection in _captureOutput.connections) {
+        for (AVCaptureInputPort *port in [connection inputPorts]) {
+            if ([[port mediaType] isEqual:AVMediaTypeVideo] ) {
+                videoConnection = connection;
+                break;
+            }
+        }
+        
+        if (videoConnection) {
+            break;
+        }
+    }
+    [_captureOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+       
+       CFDictionaryRef exifAttachments = CMGetAttachment(imageDataSampleBuffer, kCGImagePropertyExifDictionary, nil);
+        if (exifAttachments) {
+//
+        }
+        NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+        UIImage *t_image = [[UIImage alloc] initWithData:imageData];
+        _photo = [[UIImage alloc]initWithCGImage:t_image.CGImage scale:1.5 orientation:UIImageOrientationRight];
+        UIImageView *imageView = [[UIImageView alloc]initWithFrame:_previewLayer.frame];
+        [_previewLayer removeFromSuperlayer];
+        imageView.image = _photo;
+        imageView.contentMode = UIViewContentModeScaleAspectFill;
+        imageView.clipsToBounds = YES;
+        [self.view addSubview:imageView];
+        
+        
+    }];
     
 }
 
 // 切换闪光灯模式
 - (void)switchFlashMode:(UIButton*)sender {
-    
+    Class captureDeviceClass = NSClassFromString(@"AVCaptureDevice");
+    if (!captureDeviceClass) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示信息" message:@"您的设备没有拍照功能" delegate:nil cancelButtonTitle:NSLocalizedString(@"Sure", nil) otherButtonTitles: nil];
+        [alert show];
+        return;
+    }
+    NSString *imgName = @"";
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    [device lockForConfiguration:nil];
+    if ([device hasFlash]) {
+        if (device.flashMode == AVCaptureFlashModeOff) {
+            device.flashMode = AVCaptureFlashModeOn;
+            imgName = @"";
+        } else if (device.flashMode == AVCaptureFlashModeOn) {
+            device.flashMode = AVCaptureFlashModeAuto;
+            imgName = @"";
+        } else if (device.flashMode == AVCaptureFlashModeAuto) {
+            device.flashMode = AVCaptureFlashModeOff;
+            imgName = @"";
+        }
+        if (sender) {
+            [sender setImage:[UIImage imageNamed:imgName] forState:UIControlStateNormal];
+        }
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示信息" message:@"您的设备没有闪光灯功能" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles: nil];
+        [alert show];
+    }
+    [device unlockForConfiguration];
     
 }
 // 初始化相机
 
 - (void)setupCamera{
     
+    AVCaptureDevice *captureDevice = [self getVideoInputCamera:(_isFontCamera ? isFrontCamera : isBackCamera)];
+    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:nil];
+    if ([self.session canAddInput:deviceInput]){
+        [_session addInput:deviceInput];
+    }
     
+    // 预览视图(取景器)
+    _previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.session];
+    [_previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    
+    CALayer *preLayer = [[self view] layer];
+    [preLayer setMasksToBounds:YES];
+    [_previewLayer setFrame:CGRectMake(10, 100, 300, 300)];
+    [preLayer insertSublayer:_previewLayer atIndex:0];
+    [_session startRunning];
+    
+    // 创建一个输出设备,并将它添加到会话
+    _captureOutput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys:AVVideoCodecJPEG,AVVideoCodecKey,nil];
+    _captureOutput.outputSettings = outputSettings;
+    [_session addOutput:_captureOutput];
 }
 #pragma mark VideoCapture
 - (AVCaptureDevice *)getVideoInputCamera:(cameraType )cameraType{
     
-    
+    //获取(后置/前置)摄像头设备
+    NSArray *cameras = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    for (AVCaptureDevice *device in cameras){
+        switch (cameraType) {
+            case isFrontCamera:
+                if (device.position == AVCaptureDevicePositionFront)
+                    return device;
+                break;
+            case isBackCamera:
+                if (device.position == AVCaptureDevicePositionBack)
+                    return device;
+                break;
+            default:
+                break;
+        }
+    }
     return  [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
 }
 - (void)didReceiveMemoryWarning {
